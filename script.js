a4ad117b5b60cbb276b5d2c04e52dd97ba17ad4c
eunice-kim <yeeun02@gmail.com>
2025-01-17 12:47:15 -0800

    limit click rate

--- script.js
+++ script.js
@@ -7,26 +7,34 @@ const contact = document.getElementById('contact');
 homeCircle.addEventListener("click", showInfo);
 infoCircle.addEventListener("click", hideInfo);
 
+var lastClick = 0;
+
 function showInfo() {
-  infoContainer.style.display="flex";
-  setTimeout(function() {
-    infoContainer.style.opacity=1;
+  if (Date.now() - lastClick > 2000) {
+    infoContainer.style.display="flex";
     setTimeout(function() {
-      info.style.opacity=1;
+      infoContainer.style.opacity=1;
       setTimeout(function() {
-        contact.style.opacity=1;
+        info.style.opacity=1;
+        setTimeout(function() {
+          contact.style.opacity=1;
+        },500);
       },500);
-    },500);
-  },50);
+    },50);
+    lastClick = Date.now();
+  }
 }
 
 function hideInfo() {
-  info.style.opacity=0;
-  contact.style.opacity=0;
-  setTimeout(function() {
-      infoContainer.style.opacity=0;
-      setTimeout(function() {
-        infoContainer.style.display="none";
-      },1000);
-  },1000);
+  if (Date.now() - lastClick > 1050) {
+    info.style.opacity=0;
+    contact.style.opacity=0;
+    setTimeout(function() {
+        infoContainer.style.opacity=0;
+        setTimeout(function() {
+          infoContainer.style.display="none";
+        },1000);
+    },1000);
+    lastClick = Date.now();
+  }
 }
