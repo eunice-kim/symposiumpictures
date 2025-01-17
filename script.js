d17636d64ffa97b4a376ff08a9241eb432ca7a49
eunice-kim <yeeun02@gmail.com>
2025-01-17 11:59:47 -0800

    add contact

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ const homeCircle = document.getElementById('home-circle');
 const infoCircle = document.getElementById('info-circle');
 const infoContainer = document.getElementById('info-container');
 const info = document.getElementById('info');
+const contact = document.getElementById('contact');
 
 homeCircle.addEventListener("click", showInfo);
 infoCircle.addEventListener("click", hideInfo);
 
 function showInfo() {
-  console.log('showInfo');
   infoContainer.style.display="flex";
   setTimeout(function() {
     infoContainer.style.opacity=1;
     setTimeout(function() {
       info.style.opacity=1;
+      setTimeout(function() {
+        contact.style.opacity=1;
+      },250);
     },500);
   },50);
 }
 
 function hideInfo() {
-  console.log('hideInfo');
   info.style.opacity=0;
+  contact.style.opacity=0;
   setTimeout(function() {
       infoContainer.style.opacity=0;
       setTimeout(function() {
